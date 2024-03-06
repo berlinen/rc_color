02eeda564569af1d0a250a39c2423974d0255f45
berlinen <1292393108@11.com>
2024-03-06 10:54:16 +0800

    feat: impl FlavourColors

--- src/flavour_colors.rs
+++ src/flavour_colors.rs
@@ -1,3 +1,5 @@
+use crossterm::style::Color;
+
 use crate::Color;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -68,5 +70,45 @@ impl IntoIterator for FlavourColors {
 }
 
 impl FlavourColors {
-  
+    /// Returns an iterator over (name, colour) pairs in the flavour.
+    /// This can be useful for constructing a map of the colours:
+    ///
+    /// ```rust
+    /// use std::collections::HashMap;
+    /// use catppuccin::Flavour;
+    ///
+    /// let colours: HashMap<_, _> = Flavour::Mocha.colours().into_fields_iter().collect();
+    /// println!("Red: {}", colours["red"].hex());
+    /// ```
+    pub fn into_fields_iter(self) -> std::array::IntoIter<(&'static str, Color), 26> {
+        [
+            ("rosewater", self.rosewater),
+            ("flamingo", self.flamingo),
+            ("pink", self.pink),
+            ("mauve", self.mauve),
+            ("red", self.red),
+            ("maroon", self.maroon),
+            ("peach", self.peach),
+            ("yellow", self.yellow),
+            ("green", self.green),
+            ("teal", self.teal),
+            ("sky", self.sky),
+            ("sapphire", self.sapphire),
+            ("blue", self.blue),
+            ("lavender", self.lavender),
+            ("text", self.text),
+            ("subtext1", self.subtext1),
+            ("subtext0", self.subtext0),
+            ("overlay2", self.overlay2),
+            ("overlay1", self.overlay1),
+            ("overlay0", self.overlay0),
+            ("surface2", self.surface2),
+            ("surface1", self.surface1),
+            ("surface0", self.surface0),
+            ("base", self.base),
+            ("mantle", self.mantle),
+            ("crust", self.crust),
+        ]
+        .into_iter()
+    }
 }
