954ada72e96db307bf59072117fc33b270a0f2af
berlinen <1292393108@11.com>
2024-03-06 14:11:24 +0800

    feat: write simple ratatui

--- examples/ratatui.rs
+++ examples/ratatui.rs
@@ -1,3 +1,37 @@
-fn main() {
-    print!("Hello, world!")
+use std::io::{self, stdout};
+
+use ratatui::{prelude::*, widgers::*};
+use rc_color::Flavour;
+
+fn main() -> io::Result<()> {
+    let mut terminal = Terminal::with_options(
+        CrosstermBackend::new(stdout()),
+        TerminalOptions {
+            viewport: Viewport::Inline(0),
+        },
+    )?;
+
+    for flavour in Flavour::into_iter() {
+        terminal.insert_before(4, |buf| {
+            let analogous: Vec<Span> = flavour
+                .colours()
+                .into_iter()
+                .take(14)
+                .map(|c| "██".fg(c)) // fg accepts any type that implements Into<Color>
+                .collect::<Vec<Span>>();
+            let monochromatic: Vec<Span> = flavour
+                .colours()
+                .into_iter()
+                .skip(14)
+                .map(|c| "██".fg(c)) // fg accepts any type that implements Into<Color>
+                .collect();
+
+            let width = buf.area.width;
+            Paragraph::new(flavour.name()).render(Rect::new(0, 0, width, 1), buf);
+            Paragraph::new(Line::from(analogous)).render(Rect::new(0, 1, width, 1), buf);
+            Paragraph::new(Line::from(monochromatic)).render(Rect::new(0, 2, width, 1), buf);
+        })?;
+    }
+
+    Ok(())
 }
